3c85766969ffc5cb779a3e97de0c41533e9e27fa
Priyanshu Kumar <kumarpriyanshu762@gmail.com>
2025-04-22 22:25:55 +0530

    Protect route

--- client/src/Components/common/ProtectedRoute.jsx
+++ client/src/Components/common/ProtectedRoute.jsx
@@ -6,13 +6,12 @@ const ProtectedRoute = ({ children }) => {
   const location = useLocation();
   const { token, isAuthenticated,user } = useSelector((state) => state.auth);
 
-  if (isAuthenticated==null || token==null) {
+  if ((isAuthenticated==null || token==null) && (location.pathname.includes('/dashboard'))) {
     return <Navigate to="/login" replace />;
   }
 
   //take params from url and check if user is admin or ngo
   if(isAuthenticated&&(location.pathname.includes('/login')||location.pathname.includes('/register'))){
-
     if(user?.role=='admin'&&user?.role=='ngo'){
       return <Navigate to="/dashboard" replace />;
     }else{
